465786eadc2e1c323efc671e35ea3cf0bb1cc164
Ronak Jain <jain.ronak197@gmail.com>
2019-11-17 00:24:04 +0530

    Added onboarding page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:dearly/profile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
 import 'package:dearly/homepage.dart';
 import 'package:dearly/message_view.dart';
+import 'package:dearly/walkthrough/providers/color_provider.dart';
+import 'package:dearly/walkthrough/screens/onboarding/onboarding.dart';
+import 'package:dearly/walkthrough/themes/styles.dart';
+import 'package:provider/provider.dart';
 
 void main(){
   SystemChrome.setPreferredOrientations([
@@ -18,10 +23,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppBuilder(builder: (context) {
       return MaterialApp(
+        routes: <String, WidgetBuilder> {
+          '/homepage': (BuildContext context) => new HomePage(),
+        },
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: UserProfile.walkthroughLoaded == true ? HomePage() : WalkthroughStart(),
       );
     });
+  }
+}
+
+class WalkthroughStart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChangeNotifierProvider(
+        builder: (context) => ColorProvider(),
+        child: Onboarding(),
+      ),
+    );
   }
 }
 
